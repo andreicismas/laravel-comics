3dd3e79a4c5d870242ec9f402803933a42531557
andreicismas <andrei_cismas@yahoo.it>
2021-06-21 00:58:56 +0200

    add jumbotron main

--- resources/views/layouts/homeHeader.blade.php
+++ resources/views/layouts/homeHeader.blade.php
@@ -19,5 +19,54 @@
             </ul>
         </div>
     </div>
+    <div class="section_nav_2">
+        <div class="logo_nav">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="DC-logo" >
+        </div>
+
+        <div class="nav_links">
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a> 
+                </li>
+                <li>
+                    <a href="#">COMICS</a>                            
+                </li>
+                <li>
+                    <a href="#">MOVIES</a> 
+                </li>
+                <li>
+                    <a href="#">TV</a>                            
+                </li>
+                <li>
+                    <a href="#">GAMES</a> 
+                </li>
+                <li>
+                    <a href="#">COLLECTLIBLES</a>                            
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a> 
+                </li>
+                <li>
+                    <a href="#">FANS</a>                            
+                </li>
+                <li>
+                    <a href="#">NEWS</a>                            
+                </li>
+                <li>
+                    <a href="#">
+                        SHOP <span><i class="fa fa-sort-desc" aria-hidden="true"></i></span>
+                    </a>                            
+                </li>
+            </ul>
+        </div>
+        <div class="search_style">  
+            <input type="search" placeholder="search">
+            <span>
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </span>
+        
+        </div>
+    </div>
 </nav>
 </header>
---
+++ resources/views/layouts/homeMain.blade.php
@@ -0,0 +1,9 @@
+<main>
+    <section class="section_1">
+        <div class="jumbotron_style">   
+            <img src="{{asset('images/jumbotron.jpg')}}" alt="e-book">
+        </div> 
+    </section>
+    <section class=""></section>
+    <section class=""></section>
+</main>
