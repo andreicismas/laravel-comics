268eb3f637b4e8905a24f65940c4057842344087
andreicismas <andrei_cismas@yahoo.it>
2021-06-20 23:19:15 +0200

    add folder img link css

---
+++ resources/views/layouts/homeHeader.blade.php
@@ -0,0 +1,23 @@
+{{-- @extends('./welcome') --}}
+@section('title', 'ciao')
+<header>
+<nav>
+    <div class="section_nav_1">
+        <div class="white_space">
+        </div>
+        
+        <div>
+            <ul>
+                <li>
+                    DC POWER&#8480; VISA&reg;
+                </li>
+                <li>
+                    <a href="#">
+                        ADDITIONAL DC SITES <span><i class="fa fa-sort-desc" aria-hidden="true"></i></span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</header>
